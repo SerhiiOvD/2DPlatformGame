891cfbe05dcbc80e091e1d4c04e87ec565246e21
Serhii <ovodsergej@gmail.com>
2026-01-11 16:58:45 +0100

    Add two enemies (Troll, Dagon)
    Added for dragon attack sequance, improvements and refactoring.

--- Assets/DevAssets/Scripts/Core/Enemy/EnemyBase/Enemy.cs
+++ Assets/DevAssets/Scripts/Core/Enemy/EnemyBase/Enemy.cs
@@ -3,18 +3,21 @@ using UnityEngine;
 using Zenject;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
     private EnemyStateMachine _enemyStateMachine;
 
-    [SerializeField] private Rigidbody2D _rigidBody;
-    [SerializeField] private float _walkSpeed = 5f;
-    [SerializeField] private float _rangeAttack = 5f;
+    [SerializeField] protected Rigidbody2D _rigidBody;
+    [SerializeField] protected float _walkSpeed = 5f;
+    [SerializeField] protected float _rangeAttack = 5f;
+    [Tooltip("In seconds.")][SerializeField] protected int _attackSequance = 1;
+
+    protected bool _canAttack = true;
 
     public Rigidbody2D RigidBody => _rigidBody;
     public EnemyStateMachine EnemyStateMachine => _enemyStateMachine;
 
-    [Inject] private readonly Player _player;
+    [Inject] protected readonly Player _player;
 
     private void OnValidate()
     {
@@ -25,7 +28,7 @@ public class Enemy : MonoBehaviour
     {
         _enemyStateMachine = new EnemyStateMachine(this);
 
-        EnemyStateMachine.Initialize(EnemyStateMachine.WalkState);
+        EnemyStateMachine.Initialize(EnemyStateMachine.IdleState);
     }
 
     private void Update()
@@ -33,21 +36,18 @@ public class Enemy : MonoBehaviour
         EnemyStateMachine.Update();
     }
 
-    public void Idle()
+    public virtual void Idle()
     {
-        // Debug.Log("Play Idle anim");
+        
     }
-
-    public void ChaseThePlayer()
+    public virtual void ChaseThePlayer()
     {
         Vector2 dirToPlayer = (_player.transform.position - transform.position).normalized;
         _rigidBody.linearVelocity = dirToPlayer * _walkSpeed;
     }
-
-    public void Attack()
-    {
-        // Debug.Log("Enemy Attack!");
-    }
+    
+    public abstract void Attack();
+    
 
     public bool IsPlayerActive() => _player.isActiveAndEnabled;
     public bool IsDistanceToAttack() => Vector2.Distance(transform.position, _player.transform.position) < _rangeAttack;
